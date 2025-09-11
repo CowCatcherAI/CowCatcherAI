02808e0a2d5017845b6d65aab7c8ae95b3b7d530
Jacobs Farm <jacobsfarmextras@gmail.com>
2025-09-11 22:17:28 +0200

    Update config.py
    # common second rtsp camera

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 # Camera 1 - position?
 RTSP_URL_CAMERA1 = "rtsp://admin:YourPassword123@192.168.178.21:554/h264Preview_01_sub"
 # Camera 2 - position?
-RTSP_URL_CAMERA2 = "rtsp://admin:YourPassword123@192.168.178.22:554/h264Preview_01_sub"
+#RTSP_URL_CAMERA2 = "rtsp://admin:YourPassword123@192.168.178.22:554/h264Preview_01_sub"
 
 # Adding more cameras - copy and fill in rtsp://
 # RTSP_URL_CAMERA3 = "rtsp://admin:YourPassword123@192.168.178.25:554/h264Preview_01_sub"
