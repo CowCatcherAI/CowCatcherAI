a72d02f2668eab0ee3532ebb55c2cf7a8c4dc58c
Jacobs Farm <jacobsfarmextras@gmail.com>
2025-09-24 22:06:28 +0200

    Update config.py
    Added path to computervision model

--- config.py
+++ config.py
@@ -11,6 +11,9 @@ RTSP_URL_CAMERA2 = "rtsp://admin:YourPassword123@192.168.178.22:554/h264Preview_
 # RTSP_URL_CAMERA3 = "rtsp://admin:YourPassword123@192.168.178.25:554/h264Preview_01_sub"
 # RTSP_URL_CAMERA4 = "rtsp://admin:YourPassword123@192.168.178.26:554/h264Preview_01_sub"
 
+# Computervision model version
+MODEL_PATH = "CowcatcherVx.pt"
+
 # Telegram Bot Configuration
 TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Get from @BotFather on Telegram
 TELEGRAM_CHAT_ID = ["PRIMARY_CHAT_ID", "SECONDARY_CHAT_ID", "THIRD_CHAT_ID"]  # Add multiple users - Get ID from @userinfobot
